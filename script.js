024a68fc258a20565c16ecbed7bb64dadb5413fd
iagotorresg13-cpu <iago.torresg13@gmail.com>
2026-05-25 08:25:13 -0300

    Implement explore button, form submission, and custom cursor0
    Added event listeners for exploring buttons, form submission, and custom cursor. Implemented functionality for opening and closing panels with data display.

--- script.js
+++ script.js
@@ -1,1 +1,87 @@
+/* BOTÃO EXPLORAR */
 
+const btnExplorar = document.getElementById("btnExplorar");
+
+if(btnExplorar){
+
+    btnExplorar.addEventListener("click", ()=>{
+
+        const cards = document.querySelector(".cards");
+
+        cards.scrollIntoView({
+            behavior:"smooth"
+        });
+
+    });
+
+}
+
+/* FORMULÁRIO */
+
+const form = document.getElementById("formContato");
+
+if(form){
+
+    form.addEventListener("submit", (e)=>{
+
+        e.preventDefault();
+
+        alert("Mensagem enviada com sucesso!");
+
+        form.reset();
+
+    });
+
+}
+
+/* CURSOR PEIXE */
+
+const peixe = document.createElement("div");
+
+peixe.classList.add("peixe-cursor");
+
+document.body.appendChild(peixe);
+
+document.addEventListener("mousemove", (e)=>{
+
+    peixe.style.left = e.clientX + "px";
+
+    peixe.style.top = e.clientY + "px";
+
+});
+
+/* ABRIR */
+
+pontos.forEach((ponto)=>{
+
+    ponto.addEventListener("click", ()=>{
+
+        paisNome.innerText =
+        ponto.dataset.pais;
+
+        peixeNome.innerText =
+        ponto.dataset.peixe;
+
+        equipamentoNome.innerText =
+        ponto.dataset.equipamento;
+
+        dicaNome.innerText =
+        ponto.dataset.dica;
+
+        painel.classList.add("ativo");
+
+    });
+
+});
+
+/* FECHAR */
+
+if(fecharPainel){
+
+    fecharPainel.addEventListener("click", ()=>{
+
+        painel.classList.remove("ativo");
+
+    });
+
+}
